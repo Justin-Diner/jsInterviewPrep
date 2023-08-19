af04245d3190882f9db87a89955575184dd763d1
Justin <90215009+DinerJ@users.noreply.github.com>
2023-08-19 14:26:10 -0400

    IIFE

--- jsInterviewPrep.js
+++ jsInterviewPrep.js
@@ -1,1 +1,31 @@
-testing.lol
+// Function Declaration 
+
+// Q1 - What is Function Declarations? 
+//(function square(num) {
+//  return num * num
+//})()
+
+// Q2 - What is Function Expression? 
+// Store a function inside a variable. 
+//const square = function(num) {
+//  console.log(num * num); 
+//}
+
+// The part after square is an anonymous function because it has no name. 
+
+// Q3 - What are First Class Functions? 
+// In a language where a function can be treated like a variable. These are first-class functions. 
+// It can do anything that a variable can do. Functions are first class functions in JS. 
+//function displaySquare(fn) {
+//  console.log("Square is " + fn(5))
+//}
+
+//displaySquare(square);
+
+// Q4 - What is IIFE? 
+// Immediately Invoked Function Expressions 
+(function square(num) {
+  console.log(num * num)
+})(5);
+// This is immediately invoked. 
+
