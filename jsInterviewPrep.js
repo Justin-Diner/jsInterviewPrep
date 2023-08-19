292e88234f14b527b6786e58acbb55fc5e43601a
Justin <90215009+DinerJ@users.noreply.github.com>
2023-08-19 14:44:03 -0400

    more questions

--- jsInterviewPrep.js
+++ jsInterviewPrep.js
@@ -28,4 +28,60 @@
   console.log(num * num)
 })(5);
 // This is immediately invoked. 
+(function(x) {
+  return (function (y) {
+    console.log(x) // 1
+  })(2)
+})(1)
+// This gives the output of 1. Searches the innerscope. Then it searches the parent's scope. Since x exists there it finds it. This is CLOSURE. 
+// The ability of a function to access variables outside of its scope is closure. 
+
+// Q7 - Function Scope - O/P Based Question
+for (let i = 0; i < 5; i++) {
+  setTimeout(function() {
+    console.log(i)
+  }, i * 1000)
+}
+// Every time the variable runs it creates a new block scope for the function. If it has let, it has block scope. If it is var, which does not have block scope, it will produce 5, 5, 5, 5, 5. 
+
+// Hoisting 
+// The below works because functions are hoisted completely. It does not work with variables. 
+functionName()
+
+function functionName() {
+  console.log("Roadside Coder")
+}
+
+// This does not work because the variable is not hoisted. FUNCTIONS are hoisted compeltely. 
+//console.log(x);
+//x = 12
+
+// When a variable is within a local scope, you will not check the global scope. 
+
+// Functions in JavaScript
+// Q10 - Params vs Arguments 
+
+              // params
+//function square(num) {
+//  console.log(num * num);
+//}
+
+//square(5) Arguments 
+
+// Spread vs Rest Operators 
+
+//                  REST OPERATOR
+//function multiply(...nums) {
+//  console.log(nums);
+//}
+
+//var arr = [5, 6]
+//multiply(...arr) // SPREAD OPERATOR 
+// Rest must be the last parameter. 
+
+// Q12 - Callback functions
+// A function passed into another function as an argument. 
+// Examples - Map Reduce Event Listeners. 
+
+// Q13 - Arrow Functions
 
